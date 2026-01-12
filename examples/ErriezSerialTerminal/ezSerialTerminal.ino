b0893cefe716df337a1c89752de87e28f82a9a83
Alex Wong <alw1746@users.noreply.github.com>
2026-01-12 18:18:18 +1100

    Change include from ErriezSerialTerminal.h to ezSerialTerminal.h

--- examples/ErriezSerialTerminal/ezSerialTerminal.ino
+++ examples/ErriezSerialTerminal/ezSerialTerminal.ino
@@ -29,7 +29,7 @@
  *      Documentation:  https://erriez.github.io/ErriezSerialTerminal
  */
 
-#include <ErriezSerialTerminal.h>
+#include <ezSerialTerminal.h>
 
 // LED pin
 #define LED_PIN     LED_BUILTIN
